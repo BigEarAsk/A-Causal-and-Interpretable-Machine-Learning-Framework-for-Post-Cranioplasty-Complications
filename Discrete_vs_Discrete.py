66f8e575350d1b50ef0a1aa5c8adf34a9c4acd55
BigEarAsk <91580869+BigEarAsk@users.noreply.github.com>
2026-01-14 19:28:21 +0800

    Update Discrete_vs_Discrete.py

--- Discrete_vs_Discrete.py
+++ Discrete_vs_Discrete.py
@@ -9,7 +9,7 @@ if __name__ == '__main__':
     data = pd.read_excel('xxxx.xlsx',0)
     
     # get theil-U correlation matrix
-    # [1,2,3,4,5,6,7,8,9,10,11,12,14,16,17,18,23] is the column_id of discrete variables
+    # [1,2,3,4,5,6,7,8,9,10,11,12,14,16,17,18,23] are the column_ids of discrete variables
     res = associations(data[data.columns[
         [1,2,3,4,5,6,7,8,9,10,11,12,14,16,17,18,23]
     ]],nom_nom_assoc = 'theil',annot=False,vmin=0.0,plot=False)
